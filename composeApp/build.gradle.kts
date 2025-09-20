bb002bb41a285294db213d456b9d962a74f8f338
Pedro Pessoa <pedropessoaramos@gmail.com>
2025-09-20 19:00:12 -0300

    feat: Icons

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -49,6 +49,7 @@ kotlin {
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.json)
             implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.material.icons)
         }
         iosMain.dependencies {
             implementation(libs.kermit)
