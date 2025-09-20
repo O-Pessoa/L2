72f7ee7a1aaf9df5ceee7258b49fec57cb1bd304
Pedro Pessoa <pedropessoaramos@gmail.com>
2025-09-19 21:51:01 -0300

    chore: remover códigos não utilizados do boilerplate

--- composeApp/src/commonMain/kotlin/com/l2code/tmdb/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.l2code.tmdb
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/l2code/tmdb/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.l2code.tmdb
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
