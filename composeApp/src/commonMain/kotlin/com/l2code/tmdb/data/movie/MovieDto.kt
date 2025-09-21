03f3cb65750353a77d0077ec25d32802460e72e5
Pedro Pessoa <pedropessoaramos@gmail.com>
2025-09-21 01:59:46 -0300

    fix: poster_path null e backdrop_path null

--- composeApp/src/commonMain/kotlin/com/l2code/tmdb/data/movie/MovieDto.kt
+++ composeApp/src/commonMain/kotlin/com/l2code/tmdb/data/movie/MovieDto.kt
@@ -6,14 +6,14 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class MovieDto(
     val adult: Boolean,
-    val backdrop_path: String,
+    val backdrop_path: String?,
     val genre_ids: List<Int>,
     val id: Int,
     val original_language: String,
     val original_title: String,
     val overview: String,
     val popularity: Double,
-    val poster_path: String,
+    val poster_path: String?,
     val release_date: String,
     val title: String,
     val video: Boolean,
@@ -23,14 +23,14 @@ data class MovieDto(
 
     fun toEntity() = Movie(
         this.adult,
-        this.backdrop_path,
+        this.backdrop_path ?: "/",
         this.genre_ids,
         this.id,
         this.original_language,
         this.original_title,
         this.overview,
         this.popularity,
-        this.poster_path,
+        this.poster_path ?: "/",
         this.release_date,
         this.title,
         this.video,
